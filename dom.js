0accf48e19b7b7a4053a0c80edda4b9cbd9209c7
Najam-Unnisa <najamunnisa00@gmail.com>
2023-06-15 00:31:39 +0530

    creating dom file

---
+++ dom.js
@@ -0,0 +1,19 @@
+// Examine the document object//
+//console.dir(document);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.head);
+//console.log(document.doctype);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.forms[0]);
+//console.log(document.links);
+//console.log(document.images);
+
+//get element by id//
+//console.log(document.getElementById('header-title'));
+var htt = document.getElementById("main-header")
+htt.style.borderBottom = 'solid 3px #000';
+var xy = document.getElementById("main")
+xy.style.fontWeight='bold'
+xy.style.color='green'
