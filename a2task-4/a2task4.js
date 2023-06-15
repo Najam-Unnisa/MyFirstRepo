03083f37f18a83d43967b3fa04902c22fb73df1f
Najam-Unnisa <najamunnisa00@gmail.com>
2023-06-15 10:36:15 +0530

    get element by classname updated file

---
+++ a2task-4/a2task4.js
@@ -0,0 +1,8 @@
+
+//Get elements by classname
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight='bold';
+}
