8c4270e0f579651d39c630eaba2af7f25d4a29a2
Najam-Unnisa <najamunnisa00@gmail.com>
2023-06-15 11:13:41 +0530

    get element by tagname

---
+++ a2task-5/a2task5.js
@@ -0,0 +1,14 @@
+var items=document.getElementsByClassName('list-group-item');
+
+
+for(var i=0; i<items.length; i++){
+    items[i].style.backgroundColor='#ccc';
+}
+
+// get elements by tagname
+var li=document.getElementsByTagName('li');
+
+
+for(var i=0; i<li.length; i++){
+    li[i].style.backgroundColor='#ccc';
+}
